31b8b152c6bf419cd29411486497af6b8f276624
RamaLingeswaraRao950 <princeram950@gmail.com>
2026-05-19 21:43:42 +0530

    Remove duplicate nested project

--- CalVault/app/src/main/java/com/calvault/app/callbacks/DialogActionsCallback.kt
+++
@@ -1,7 +0,0 @@
-package com.calvault.app.callbacks
-
-interface DialogActionsCallback {
-    fun onPositiveButtonClicked()
-    fun onNegativeButtonClicked()
-    fun onNaturalButtonClicked()
-}
--- CalVault/app/src/main/java/com/calvault/app/callbacks/FileProcessCallback.kt
+++
@@ -1,8 +0,0 @@
-package com.calvault.app.callbacks
-
-import java.io.File
-
-interface FileProcessCallback {
-    fun onFilesProcessedSuccessfully(copiedFiles: List<File>)
-    fun onFileProcessFailed()
-}
